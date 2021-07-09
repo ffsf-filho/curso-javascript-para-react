506cc1045bf05a11ffb65620e2048d62c2537dea
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-07-08 22:37:45 -0300

    :sparkles: Finish the BoardGame object

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 import './src/style/generic/reset.css';
 import './src/style/settings/colors.css';
 import './src/style/elements/base.css';
-import './style.css';
+//import './style.css';
 import BoardGame from './src/Objects/BoardGame';
 
 const $root = document.querySelector("#root");
