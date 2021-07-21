634c6ad5d4143052389f7e9ef8e64a18ac2a4018
Francisco Florentino de Sousa Filho <ffsf.filho@gmail.com>
2021-07-21 13:51:09 -0300

    :sparkles: Add flip CardGame

--- src/components/CardGame/index.js
+++ src/components/CardGame/index.js
@@ -1,11 +1,11 @@
 import './style.css';
 
 function CardGame(icon = "alura-pixel", alt = "Logo da Alura"){
-  	return `
-      <article class='card-game'>
-        <img src="images/${icon}.svg" alt="${alt}">
-      </article>
-    `;
+  return `
+    <article class='card-game'>
+      <img src="images/${icon}.svg" alt="${alt}">
+    </article>
+  `;
 }
 
 export default CardGame;
